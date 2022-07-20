394549b8f2f6b1cc95628e365585d3dc0e5bcb83
mcmenamc <mcmena636@gmail.com>
2022-07-19 20:05:01 -0500

    Inicio del proyecto

---
+++ ClassCapaNegocio/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCapaNegocio
+{
+    public class Class1
+    {
+    }
+}
