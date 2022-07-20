07992f70aff21ad91e6ba0405eb2de82f08c5789
mcmenamc <mcmena636@gmail.com>
2022-07-20 16:20:12 -0500

    Se agregaron las Entidades necesarias de la base de datos

---
+++ ClassCapaEntidades/Cuatrimestre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCapaEntidades
+{
+    public class Cuatrimestre
+    {
+        public int id_Cuatrimestre { get; set; }
+        public string Periodo { get; set; }
+        public int Anio { get; set; }
+        public int Inicio { get; set; }
+        public int Fin { get; set; }
+        public int Extra { get; set; }
+    }
+}
---
+++ ClassCapaEntidades/Medico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCapaEntidades
+{
+    public class Medico
+    {
+        public int ID_Dr { get; set; }
+        public string Nombre { get; set; }
+        public string App { get; set; }
+        public string Apm { get; set; }
+        public string Telefono { get; set; }
+        public string correo { get; set; }
+        public string especialidad { get; set; }
+        public string extra { get; set; }
+    }
+}
