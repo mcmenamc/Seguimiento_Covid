2e545a9b4dc726d321b84d0cb55750bb4c0bb0f3
mcmenamc <mcmena636@gmail.com>
2022-07-20 15:15:00 -0500

    Se realizo la capa de acceso a datos con los métodos necesario para su correcto funcionamiento de la aplicación.

---
+++ ClasscCapaDatos/ClassAccesoSQL.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace ClasscCapaDatos
+{
+    public class ClassAccesoSQL
+    {
+        // Propiedad privada que almacenará la conexión de la base de datos
+        private SqlConnection Connection;
+        // Método que realiza la conexión a SQL Server ocupando el Web.config
+        private void OpenConnection()
+        {
+            string conn_string = ConfigurationManager.ConnectionStrings["conn_covid"].ConnectionString;
+            this.Connection = new SqlConnection(conn_string);
+            this.Connection.Open();
+        }
+        // 
+        public DataSet QueryDataSet(string querySql, ref string message, List<SqlParameter> listParameter)
+        {
+            SqlCommand command = null;
+            SqlDataAdapter adapter = null;
+            DataSet dataSet = new DataSet();
+
+            if (this.Connection == null)
+            {
+                message = "La conexión con la base de datos no ha sido exitosa";
+                dataSet = null;
+            }
+            else
+            {
+                command = new SqlCommand(querySql, this.Connection);
+                adapter = new SqlDataAdapter(command);
+
+                foreach (SqlParameter parameter in listParameter)
+                    command.Parameters.Add(parameter);
+
+                try
+                {
+                    adapter.Fill(dataSet, "Consulta");
+                    message = "El DataSet se lleno";
+                }
+                catch (Exception a)
+                {
+                    message = "Error: " + a.Message;
+                }
+
+                this.Connection.Close();
+                this.Connection.Dispose();
+            }
+            return dataSet;
+        }
+        // Método que retorna una DataTable de una consulta SQL, igualmente se tomó en cuenta la seguridad a la hora de las inyecciones SQL 
+        public DataTable QueryDataTable(string querySql, ref string message, List<SqlParameter> listParameter)
+        {
+            SqlCommand command = null;
+            DataTable table = null;
+            OpenConnection();
+
+            if (this.Connection == null)
+            {
+                message = "No hay conexion a la BD";
+                table = null;
+            }
+            else
+            {
+                command = new SqlCommand(querySql, this.Connection);
+
+                foreach (SqlParameter p in listParameter)
+                {
+                    command.Parameters.Add(p);
+                }
+
+                try
+                {
+                    table = new DataTable();
+                    table.Load(command.ExecuteReader());
+                    command.Dispose();
+                    message = "Consulta Correcta con DataReader un total de filas " + table.Rows.Count;
+                }
+                catch (Exception ex)
+                {
+                    table = null;
+                    message = "Error: " + ex.Message;
+                }
+                this.Connection.Close();
+                this.Connection.Dispose();
+            }
+            return table;
+        }
+        // A la hora de realizar una modificación en la base de datos no necesitamos devolver una tabla, si no una bandera que nos ayudará a saber si lo realizo correctamente 
+        public Boolean modification(string querySql, ref string mensaje, List<SqlParameter> listParameter)
+        {
+            Boolean flag = false;
+            SqlCommand command = null;
+            this.OpenConnection();
+
+            if (this.Connection != null)
+            {
+                command = new SqlCommand();
+                command.CommandText = querySql;
+                command.Connection = this.Connection;
+
+                foreach (SqlParameter p in listParameter)
+                    command.Parameters.Add(p);
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                    flag = true;
+                    mensaje = "Modificación correcta.";
+                }
+                catch (Exception f)
+                {
+                    flag = false;
+                    mensaje = "ERROR: " + f.Message;
+                }
+            }
+            else
+            {
+                flag = false;
+                mensaje = "No hay conexión a la BD.";
+            }
+            return flag;
+        }
+    }
+}
---
+++ WebApplication/Views/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Page.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication.Views.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    <br />
+
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+
+    <br />
+    <br />
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
+
+
+
+
+</asp:Content>
